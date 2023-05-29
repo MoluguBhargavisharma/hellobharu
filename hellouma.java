28663c79ac08217d054df793130517cbc68c66f6
MoluguBhargavisharma <102900168+MoluguBhargavisharma@users.noreply.github.com>
2023-05-29 16:35:07 +0530

    Create hellouma.java

---
+++ hellouma.java
@@ -0,0 +1,11 @@
+package mavenmindtree;
+
+public class hellouma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BHARGAVI,VAISHNAVI");
+
+	}
+
+}
